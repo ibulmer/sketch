8c0298458b31c84ce172fd7f8d544fa9d03fcd31
Ian <ian.bulmer89@gmail.com>
2015-10-02 02:05:45 -0400

    Made several changes in order to remove the spaces between elements.

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
     var myHtml = "";
     var myCSS = "";
-    var x = 5;
-    var y = 5; 
+    var x = 25;
+    var y = 25; 
+    myHtml+="<table>";
     for (var counter = 0; counter < x; counter++) {
-    for(var col=0; col<y; col++){
-	myHtml+= '<div class="cow"></div>' +"\n"
+        myHtml+="<tr>";
+      for(var col=0; col<y; col++){
+        myHtml+= "<td class='cow'></td>";
+      }
+      myHtml+="</tr>";
     }
-    myHtml+="<br>" +"\n";
-    
-}
+    myHtml+="</table>"
     myHtml= "<div id='box'>"+myHtml + "</div>";
-    myCSS += "<style> .cow {height: 50px; width: 50px; background-color: green; display: inline-block; opacity: 0.3; left: 600px; position: relative;}</style>"
-  
+    myCSS += "<style> #box {margin: auto; width: 50%} .cow {height:" + 500/x + "px; width:" + 500/y + "px ; background-color: green; opacity: 0.3;} table, tr, td {margin: 0; border: 1px solid black; padding: 0; border-collapse:collapse;}</style>"
     $('body').append(myHtml);
     $('head').append(myCSS);
     $('.cow').mouseenter(function(){
@@ -28,19 +29,25 @@ $(document).ready(function(){
     var myHtml = "";
     var myCSS = "";
     var x = prompt("enter a number between 1 and 50");
-    var y = prompt("enter a number between 1 and 50"); 
-    var wide= 500/y
-    var tall= 500/x
-    for (var counter = 0; counter < x; counter++) {
-    for(var col=0; col<y; col++){
-	myHtml+= '<div class="cow"></div>' +"\n"
+    while (x>50){
+        x=prompt("pick a lower number!");
     }
-    myHtml+="<br>" +"\n";
-    
-}
+    var y = prompt("enter a number between 1 and 50"); 
+    while (y>50){
+        y=prompt("pick a lower number!");
+    }
+    myHtml+="<table>";
+    for (var counter = 0; counter < x; counter++) {
+        myHtml+="<tr>";
+      for(var col=0; col<y; col++){
+        myHtml+= "<td class='cow'></td>";
+      }
+      myHtml+="</tr>";
+    }
+    myHtml+="</table>"
     myHtml= "<div id='box'>"+myHtml + "</div>";
-    myCSS += "<style> .cow {height: "+tall+"px; width: "+wide+"px; background-color: green; display: inline-block; opacity: 0.3; margin: 0px; 	border: 0px; left: 600px; position: relative;}</style>";
-  
+    myCSS += "<style> .cow {height:" + 500/x + "px; width:" + 500/y + "px ; background-color: green; opacity: 0.3;} table, tr, td {margin: 0; border: 1px solid black; padding: 0; border-collapse:collapse;}</style>"
+   
     $('body').append(myHtml);
     $('head').append(myCSS);
     $('.cow').mouseenter(function(){
@@ -57,19 +64,25 @@ $('#blue_button').click(function(){
     var myHtml = "";
     var myCSS = "";
     var x = prompt("enter a number between 1 and 50");
+    while (x>50){
+        x=prompt("pick a lower number!");
+    };
     var y = prompt("enter a number between 1 and 50"); 
-    var wide= 500/y
-    var tall= 500/x
+    while (y>50){
+        y=prompt("pick a lower number!");
+    };
+    myHtml+="<table>";
     for (var counter = 0; counter < x; counter++) {
-    for(var col=0; col<y; col++){
-	myHtml+= '<div class="cow"></div>' +"\n"
+        myHtml+="<tr>";
+      for(var col=0; col<y; col++){
+        myHtml+= "<td class='cow'></td>";
+      }
+      myHtml+="</tr>";
     }
-    myHtml+="<br>" +"\n";
-    
-}
+    myHtml+="</table>"
     myHtml= "<div id='box'>"+myHtml + "</div>";
-    myCSS += "<style> .cow {height: "+tall+"px; width: "+wide+"px; background-color: blue; display: inline-block; opacity: 0.3; margin: 0px; 	border: 0px; left: 600px; position: relative;}</style>";
-  
+    myCSS += "<style> .cow {height:" + 500/x + "px; width:" + 500/y + "px ; background-color: blue; opacity: 0.3;} table, tr, td {margin: 0; border: 1px solid black; padding: 0; border-collapse:collapse;}</style>"
+   
     $('body').append(myHtml);
     $('head').append(myCSS);
     $('.cow').mouseenter(function(){
@@ -86,19 +99,25 @@ $('#yellow_button').click(function(){
     var myHtml = "";
     var myCSS = "";
     var x = prompt("enter a number between 1 and 50");
+    while (x>50){
+        x=prompt("pick a lower number!");
+    };
     var y = prompt("enter a number between 1 and 50"); 
-    var wide= 500/y
-    var tall= 500/x
+    while (y>50){
+        y=prompt("pick a lower number!");
+    };
+    myHtml+="<table>";
     for (var counter = 0; counter < x; counter++) {
-    for(var col=0; col<y; col++){
-	myHtml+= '<div class="cow"></div>' +"\n"
+        myHtml+="<tr>";
+      for(var col=0; col<y; col++){
+        myHtml+= "<td class='cow'></td>";
+      }
+      myHtml+="</tr>";
     }
-    myHtml+="<br>" +"\n";
-    
-}
+    myHtml+="</table>"
     myHtml= "<div id='box'>"+myHtml + "</div>";
-    myCSS += "<style> .cow {height: "+tall+"px; width: "+wide+"px; background-color: yellow; display: inline-block; opacity: 0.3; margin: 0px; 	border: 0px; left: 600px; position: relative;}</style>";
-  
+    myCSS += "<style> .cow {height:" + 500/x + "px; width:" + 500/y + "px ; background-color: yellow; opacity: 0.3;} table, tr, td {margin: 0; border: 1px solid black; padding: 0; border-collapse:collapse;}</style>"
+   
     $('body').append(myHtml);
     $('head').append(myCSS);
     $('.cow').mouseenter(function(){
